535309eb3755736081293290f4fa178bddd5632b
Сергей Лимонов <illustrv@MacBook-Air-Sergej.local>
2025-10-05 17:59:01 +0300

    simple storage

---
+++ internal/repository/repository.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"fmt"
+	"sync"
+)
+
+type MemStorage struct {
+	s  map[string]string
+	mu sync.RWMutex
+}
+
+func NewStrorage() *MemStorage {
+	storage := make(map[string]string, 50)
+	return &MemStorage{
+		s: storage,
+	}
+}
+
+func (m *MemStorage) Get(hash string) (string, error) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	url, exist := m.s[hash]
+	if exist {
+		return "", fmt.Errorf("%s not found", hash)
+	}
+	return url, nil
+}
+
+func (m *MemStorage) Set(url, hash string) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.s[hash] = url
+}
