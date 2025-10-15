646d88f8c782aa7bc5d16dba4c44c0e69e2af97f
Сергей Лимонов <illustrv@MacBook-Air-Sergej.local>
2025-10-15 18:38:41 +0300

    iter2

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -1,108 +1,115 @@
 package service
 
+import (
+	"fmt"
+	"testing"
 
-// type MockRepo struct {
-// 	mock.Mock
-// }
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
 
-// func newWrapService() *Service {
-// 	r := &MockRepo{}
-// 	return &Service{
-// 		repo: r,
-// 	}
-// }
+type MockRepo struct {
+	mock.Mock
+}
 
-// func (m *MockRepo) Get(hash string) (string, error) {
-// 	args := m.Called(hash)
-// 	return args.String(0), args.Error(1)
-// }
+func newWrapService() *Service {
+	r := &MockRepo{}
+	return &Service{
+		repo: r,
+	}
+}
 
-// func (m *MockRepo) Set(url, hash string) error {
-// 	args := m.Called(url, hash)
-// 	return args.Error(0)
-// }
-// func TestServiceSetURL(t *testing.T) {
-// 	tests := []struct {
-// 		name     string
-// 		url      string
-// 		wantHash string
-// 		wantErr  error
-// 		repoIsOn bool
+func (m *MockRepo) Get(hash string) (string, error) {
+	args := m.Called(hash)
+	return args.String(0), args.Error(1)
+}
 
-// 		mockHash string
-// 	}{
-// 		{
-// 			name:     "success set",
-// 			url:      "someUrl",
-// 			wantHash: "78264b7b9514f66e",
-// 			wantErr:  nil,
-// 			repoIsOn: true,
-// 			mockHash: "78264b7b9514f66e",
-// 		},
-// 		{
-// 			name:     "empty upl",
-// 			url:      "",
-// 			wantHash: "",
-// 			wantErr:  fmt.Errorf("incorrect url"),
-// 			repoIsOn: false,
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			s := newWrapService()
-// 			mockRepo := s.repo.(*MockRepo)
+func (m *MockRepo) Set(url, hash string) error {
+	args := m.Called(url, hash)
+	return args.Error(0)
+}
+func TestServiceSetURL(t *testing.T) {
+	tests := []struct {
+		name     string
+		url      string
+		wantHash string
+		wantErr  error
+		repoIsOn bool
 
-// 			if tt.repoIsOn {
-// 				mockRepo.On("Set", tt.url, tt.mockHash).Return(nil)
-// 			}
+		mockHash string
+	}{
+		{
+			name:     "success set",
+			url:      "someUrl",
+			wantHash: "78264b7b9514f66e",
+			wantErr:  nil,
+			repoIsOn: true,
+			mockHash: "78264b7b9514f66e",
+		},
+		{
+			name:     "empty upl",
+			url:      "",
+			wantHash: "",
+			wantErr:  fmt.Errorf("incorrect url"),
+			repoIsOn: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := newWrapService()
+			mockRepo := s.repo.(*MockRepo)
 
-// 			gotHash, err := s.SetURL(tt.url)
+			if tt.repoIsOn {
+				mockRepo.On("Set", tt.url, tt.mockHash).Return(nil)
+			}
 
-// 			assert.Equal(t, tt.wantHash, gotHash)
-// 			assert.Equal(t, tt.wantErr, err)
-// 		})
-// 	}
-// }
+			gotHash, err := s.SetURL(tt.url)
 
-// func TestServiceGetURL(t *testing.T) {
-// 	tests := []struct {
-// 		name     string
-// 		hash     string
-// 		wantUrl  string
-// 		wantErr  error
-// 		repoIsOn bool
+			assert.Equal(t, tt.wantHash, gotHash)
+			assert.Equal(t, tt.wantErr, err)
+		})
+	}
+}
 
-// 		mockHash string
-// 	}{
-// 		{
-// 			name:     "success get",
-// 			hash:     "78264b7b9514f66e",
-// 			wantUrl:  "someUrl",
-// 			wantErr:  nil,
-// 			repoIsOn: true,
-// 			mockHash: "78264b7b9514f66e",
-// 		},
-// 		{
-// 			name:     "empty upl",
-// 			hash:     "",
-// 			wantUrl:  "",
-// 			wantErr:  fmt.Errorf("incorrect id"),
-// 			repoIsOn: false,
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			s := newWrapService()
-// 			mockRepo := s.repo.(*MockRepo)
+func TestServiceGetURL(t *testing.T) {
+	tests := []struct {
+		name     string
+		hash     string
+		wantUrl  string
+		wantErr  error
+		repoIsOn bool
 
-// 			if tt.repoIsOn {
-// 				mockRepo.On("Get", tt.mockHash).Return(tt.wantUrl, nil)
-// 			}
+		mockHash string
+	}{
+		{
+			name:     "success get",
+			hash:     "78264b7b9514f66e",
+			wantUrl:  "someUrl",
+			wantErr:  nil,
+			repoIsOn: true,
+			mockHash: "78264b7b9514f66e",
+		},
+		{
+			name:     "empty upl",
+			hash:     "",
+			wantUrl:  "",
+			wantErr:  fmt.Errorf("incorrect id"),
+			repoIsOn: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := newWrapService()
+			mockRepo := s.repo.(*MockRepo)
 
-// 			gotUrl, err := s.GetURL(tt.hash)
+			if tt.repoIsOn {
+				mockRepo.On("Get", tt.mockHash).Return(tt.wantUrl, nil)
+			}
 
-// 			assert.Equal(t, tt.wantUrl, gotUrl)
-// 			assert.Equal(t, tt.wantErr, err)
-// 		})
-// 	}
-// }
+			gotUrl, err := s.GetURL(tt.hash)
+
+			assert.Equal(t, tt.wantUrl, gotUrl)
+			assert.Equal(t, tt.wantErr, err)
+		})
+	}
+}
