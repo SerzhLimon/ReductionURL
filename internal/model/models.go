cf8e86ccd2f6a21b0932405ea57fbb6c7fe843d7
Сергей Лимонов <illustrv@MacBook-Air-Sergej.local>
2026-02-22 15:18:13 +0300

    reset

--- internal/model/models.go
+++ internal/model/models.go
@@ -1,24 +1,29 @@
 package model
 
+// generate:reset
 type SetURLJsonRequest struct {
 	URL string `json:"url"`
 }
 
+// generate:reset
 type SetURLJsonResponse struct {
 	URL string `json:"result"`
 }
 
+// generate:reset
 type SetArrayURLRequest struct {
 	ID          string `json:"correlation_id"`
 	OriginalURL string `json:"original_url"`
 	ShortURL    string
 }
 
+// generate:reset
 type SetArrayURLResponse struct {
 	ID  string `json:"correlation_id"`
 	URL string `json:"short_url"`
 }
 
+// generate:reset
 type GetArrayURLResponse struct {
 	Short    string `json:"short_url"`
 	Original string `json:"original_url"`
